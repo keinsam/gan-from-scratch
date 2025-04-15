366c00fd98dc1ac4f6451a7d9ae90c8381e5b52c
keinsam <kein8sam@gmail.com>
2025-04-15 17:34:40 +0200

    model implementation

--- src/model.py
+++ src/model.py
@@ -1,10 +1,33 @@
 import torch
 from torch import nn
 
-class Discriminator(nn.Module) :
-    def __init__(self) :
+class Discriminator(nn.Module):
+    def __init__(self, input_dim, hidden_dim):
         super(Discriminator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, 1),
+            nn.Sigmoid()
+        )
+    
+    def forward(self, x):
+        return self.model(x)
 
-class Generator(nn.Module) :
-    def __init__(self) :
+class Generator(nn.Module):
+    def __init__(self, latent_dim, hidden_dim, output_dim):
         super(Generator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(latent_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, output_dim),
+            nn.Tanh()
+        )
+    
+    def forward(self, z):
+        return self.model(z)
+
